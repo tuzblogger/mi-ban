73996cf79b43b504e04d68f8b3f6c5f28183bc0f
tuzblogger <gayaraqelyan1712@gmail.com>
2024-01-09 14:37:13 -0800

    header is done

--- FigsCoffee/figs-coffee/src/components/Header.jsx
+++ FigsCoffee/figs-coffee/src/components/Header.jsx
@@ -1,4 +1,8 @@
 import logo from "./images/logo.png"
+import heart from "./images/heart_icon.png"
+import box from "./images/box_icon.png"
+import man from "./images/man_icon.png"
+import search from "./images/search_icon.png"
 import { useEffect, useState } from "react";
 import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
@@ -7,24 +11,29 @@ export default function Header() {
 
     return (
         <header>
-            <img src={logo} alt="" id="logo-menu"/>
-            <button>About us</button>
-            <button>Our Products</button>
-            <button>Delivery</button>
-            <div>
-                <img src="" alt="" />
-                <input type="text" />
+            <div> 
+                <img src={logo} alt="" id="logo-menu"/>
             </div>
-            <button>
-                <img src="" alt="" />
-            </button>
-            <button>
-                <img src="" alt="" />
-            </button>
-            <button>
-                <img src="" alt="" />
-            </button>
 
+            <div id="vectors">
+                <button>About us</button>
+                <button>Our Products</button>
+                <button>Delivery</button>
+                <div id="search">
+                    <img src={search} alt="" id="search_icon"/>
+                    <input type="text" placeholder="Search"/>
+
+                </div>
+                <button>
+                    <img src={heart} alt="" />
+                </button>
+                <button>
+                    <img src={box} alt="" />
+                </button>
+                <button>
+                    <img src={man} alt="" />
+                </button>
+            </div>
             
         </header>
     )
